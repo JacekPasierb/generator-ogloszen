3dbfc0a01188f71dce8dfd833ada9f4ee4b0aa37
Jacek Pasierb <jack_byk@o2.pl>
2025-05-20 00:41:56 +0200

    Feat/Auth - created auth for register and login form

---
+++ src/app/components/CardAuth/CardAuth.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import styles from "./CardAuth.module.css";
+import Image from "next/image";
+import {usePathname} from "next/navigation";
+import Title from "../Title/Title";
+import SubTitle from "../SubTitle/SubTitle";
+import LoginForm from "../LoginForm/LoginForm";
+import RegisterLink from "../RegisterLink/RegisterLink";
+import BackHomeLink from "../BackHomeLink/BackHomeLink";
+
+const CardAuth = () => {
+  const pathname = usePathname();
+  const isRegister = pathname.startsWith("/register");
+  return (
+    <div className={styles.cardAuth}>
+      <Image
+        src="/logo.png"
+        width={80}
+        height={80}
+        alt="logo"
+        className={styles.logo}
+      />
+
+      <Title>
+        {isRegister ? "Załóż darmowe konto" : "Zaloguj się do konta"}
+      </Title>
+      <SubTitle>Uzyskaj dostęp do opisów AI</SubTitle>
+      {isRegister ? "" : <LoginForm />}
+      <RegisterLink />
+      <BackHomeLink />
+    </div>
+  );
+};
+
+export default CardAuth;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardAuth from "../components/CardAuth/CardAuth";
+
+const page = () => {
+  return (
+    <section className={`section container`}>
+      <CardAuth />
+    </section>
+  );
+};
+
+export default page;
