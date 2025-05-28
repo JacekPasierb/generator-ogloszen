6392af75b937ed1300e2349c91a59837ba26ccea
Jacek Pasierb <jack_byk@o2.pl>
2025-05-28 21:57:12 +0200

    Feat/codeReview

--- src/app/components/CardProduct/CardProduct.tsx
+++ src/app/components/CardProduct/CardProduct.tsx
@@ -3,23 +3,25 @@ import Link from "next/link";
 import {benefits} from "../../data/benefit";
 
 const CardProduct = () => {
-  <div className={styles.card}>
-    <div>
-      <h3 className={styles.title}>Pakiet AI</h3>
-      <p className={styles.price}>Tylko 5 zł</p>
-    </div>
+  return (
+    <div className={styles.card}>
+      <div>
+        <h3 className={styles.title}>Pakiet AI</h3>
+        <p className={styles.price}>Tylko 5 zł</p>
+      </div>
 
-    <ul className={styles.benefitsList}>
-      {benefits.map((benefit, index) => (
-        <li key={index}>
-          <p className={styles.benefit}>{benefit}</p>
-        </li>
-      ))}
-    </ul>
-    <Link href="/register" className={styles.cta}>
-      Załóż konto i aktywuj
-    </Link>
-  </div>;
+      <ul className={styles.benefitsList}>
+        {benefits.map((benefit, index) => (
+          <li key={index}>
+            <p className={styles.benefit}>{benefit}</p>
+          </li>
+        ))}
+      </ul>
+      <Link href="/register" className={styles.cta}>
+        Załóż konto i aktywuj
+      </Link>
+    </div>
+  );
 };
 
 export default CardProduct;
