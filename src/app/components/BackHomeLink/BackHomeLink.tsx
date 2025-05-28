de073a756548322b133ebae198249299634948a8
Jacek Pasierb <jack_byk@o2.pl>
2025-05-28 21:18:07 +0200

    Fix/BackHomeLink - correct component

--- src/app/components/BackHomeLink/BackHomeLink.tsx
+++ src/app/components/BackHomeLink/BackHomeLink.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import React from "react";
-import styles from "./BackHomeLink.module.css"
+import styles from "./BackHomeLink.module.css";
 
 const BackHomeLink = () => {
   return (
-    <Link href="/" className={styles.link}>
+    <Link
+      href="/"
+      className={styles.link}
+      aria-label="Powrót na stronę główną"
+      title="Powrót na stronę główną"
+    >
       ← Wróć na stronę główną
     </Link>
   );
