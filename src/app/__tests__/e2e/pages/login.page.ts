97c2cd2a940ba137d30a67578a2901596c74aa50
Jacek Pasierb <jack_byk@o2.pl>
2025-06-20 22:19:08 +0200

    Feature/login.spec - add test for validation email

--- src/app/__tests__/e2e/pages/login.page.ts
+++ src/app/__tests__/e2e/pages/login.page.ts
@@ -5,12 +5,14 @@ export class LoginPage {
   emailInput: Locator;
   passwordInput: Locator;
   loginButton: Locator;
+  emailError: Locator;
 
   constructor(private page: Page) {
     this.toLoginButton = this.page.getByRole("link", {name: "Zaloguj się"});
     this.emailInput = this.page.getByRole("textbox", {name: "Email"});
     this.passwordInput = this.page.getByRole("textbox", {name: "Hasło"});
     this.loginButton = this.page.getByRole("button", {name: "Zaloguj"});
+    this.emailError = this.page.getByTestId("error-email");
   }
 
   async login(userEmail: string, userPassword: string): Promise<void> {
