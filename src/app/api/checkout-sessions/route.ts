1a6e35fb6e9626978fa6d106e6a62d04a85750cf
Jacek Pasierb <jack_byk@o2.pl>
2025-12-14 01:51:58 +0100

    Feat- update 1.0.1 new next version

--- src/app/api/checkout-sessions/route.ts
+++ src/app/api/checkout-sessions/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest) {
 
     const userId = getUserIdFromToken(token);
 
-    const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+    const appUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
